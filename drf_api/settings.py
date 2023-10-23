e24fdfb36b3acdb7ae4bd7241d07e4b3ef6d07c9
MikeMaroni9 <k.vaitka@gmail.com>
2023-10-23 19:45:44 +0000

    allowed hosts updated with heroku

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['8000-mikemaroni9-drfapi-2slkerxdci7.ws-eu105.gitpod.io', 'localhost', 'drf-api9.herokuapp.com']
+ALLOWED_HOSTS = ['8000-mikemaroni9-drfapi-2slkerxdci7.ws-eu105.gitpod.io', 'localhost', 'https://drf-api9-921f22751f01.herokuapp.com/']
 
 
 # Application definition
